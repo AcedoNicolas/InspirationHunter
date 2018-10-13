3c6f4c209a390a87c31540de5d909b7a1dcc6240
AcedoNicolas <acedo_nicolas@hotmail.com>
2018-10-13 21:05:25 +0200

    Index Layout
    Register and Login Layout include into Index

---
+++ Includes/Login.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<div class="login-div">
+<form method="post"> 
+	<ul>
+		<li>
+		  <input type="text" name="email" placeholder="Please enter your Email here"/>
+		</li>
+		<li>
+		  <input type="password" name="password" placeholder="password"/><input type="submit" name="login" value="Log In"/>
+		</li>
+		
+	</ul>
+	<!--
+	 <li class="error-li">
+	  <div class="span-fp-error"></div>
+	 </li> 
+	-->
+	</form>
+</div>
--- Index.php
+++ Index.php
@@ -1,3 +1,79 @@
 <?php
 include'core/database/connection.php';
 ?>
+
+<html>
+	<head>
+		<title>InspirationHunter</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
+		<link rel="stylesheet" href="assets/css/style-complete.css"/>
+	</head>
+	<!--Helvetica Neue-->
+<body>
+<div class="front-img">
+	<img src="assets/images/background.jpg"></img>
+</div>	
+
+<div class="wrapper">
+<!-- header wrapper -->
+<div class="header-wrapper">
+	
+	<div class="nav-container">
+		<!-- Nav -->
+		<div class="nav">
+			
+			<div class="nav-left">
+				<ul>
+					<li><i class="" aria-hidden="true"></i><a href="#">Home</a></li>
+					<li><a href="#"></a></li>
+				</ul>
+			</div><!-- nav left ends-->
+
+			<div class="nav-right">
+				<ul>
+					<li><a href="#"></a></li>
+				</ul>
+			</div><!-- nav right ends-->
+
+		</div><!-- nav ends -->
+
+	</div><!-- nav container ends -->
+
+</div><!-- header wrapper end -->
+	
+<!---Inner wrapper-->
+<div class="inner-wrapper">
+	<!-- main container -->
+	<div class="main-container">
+		<!-- content left-->
+		<div class="content-left">
+            <br>
+			<h1>Inspiration Hunter</h1>
+			<br/>
+			<p>Welcome on Inspiration Hunter.This is the perfect place to share all your design's with the rest of the world.
+                Just register and get started right now.
+            </p>
+		</div><!-- content left ends -->	
+
+		<!-- content right ends -->
+		<div class="content-right">
+			<!-- Log In Section -->
+			<div class="login-wrapper">
+			 <?php include 'includes/Login.php';?>
+			</div><!-- log in wrapper end -->
+
+			<!-- SignUp Section -->
+			<div class="signup-wrapper">
+            <?php include 'includes/Register.php';?>
+			</div>
+			<!-- SIGN UP wrapper end -->
+
+		</div><!-- content right ends -->
+
+	</div><!-- main container end -->
+
+</div><!-- inner wrapper ends-->
+</div><!-- ends wrapper -->
+</body>
+</html>
