2b1746ce62ab3c00ac406e46d4bfc4a1f7e4fb08
AcedoNicolas <acedo_nicolas@hotmail.com>
2018-12-04 18:39:19 +0100

    Login+Register working
    Register werkt nu op Bcrypt
    Login werkt enkel nog op md5 (STILL GOTTA FIX)

--- Core/Classes/User.php
+++ Core/Classes/User.php
@@ -4,28 +4,67 @@ class User{
 function __construct($pdo){
     $this->pdo = $pdo;
 }
+
 public function checkInput($var){//deze functie kan van overal opgeroepen worden
 $var = htmlspecialchars($var);
 $var = trim($var);//removes whitespace
 $var = stripcslashes($var); //remove slaches from string
 return $var;
 }
-public function login($email,$password){//Checken voor de gegevens in onze database voor de login
-$stmt = $this->pdo->prepare("SELECT'email' FROM 'users' WHERE 'email' = :email AND 'password' = :password");// hier voorkom je SQL injection
-$stmt->bindParam(":email",$email,PDO::PARAM_STR);
-$stmt->bindParam(":password",password_hash($password, PASSWORD_BCRYPT),PDO::PARAM_STR);
-$stmt->execute();
 
-//checken of de gevonden users minstens 1 is
-$user = $stmt->fetch(PDO::FETCH_OBJ);
-$count = $stmt->rowCount();
 
-if($count > 0){
-    $_SESSION['user_id'] = $user->user_id;
-    header('Location:Home.php');
-}else{
-   return false; 
+public function login($email, $password){//Checken voor de gegevens in onze database voor de login
+  
+    $passwordHash = password_hash($password,PASSWORD_BCRYPT);
+    $stmt = $this->pdo->prepare('SELECT `user_id` FROM `users` WHERE `email` = :email AND `password` = :password');
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindParam(':password', $passwordHash, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $count = $stmt->rowCount();
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+
+    if($count > 0){
+        $_SESSION['user_id'] = $user->user_id;
+        header('Location: home.php');
+    }else{
+        return false;
+    }
 }
+public function checkEmail($email){
+    $stmt = $this->pdo->prepare("SELECT 'email' FROM 'users' WHERE 'email' = :email  "); 
+    $stmt->bindParam(":email",$email,PDO::PARAM_STR);
+    $stmt->execute();
+
+    $count = $stmt->rowCount();
+    if($count >0){
+        return true;
+    }else{
+        return false;
+    }
 }
+
+
+public function register($email, $password, $screenName){
+    $passwordHash = password_hash($password,PASSWORD_BCRYPT);
+    $stmt = $this->pdo->prepare("INSERT INTO `users` (`email`, `password`, `screenName`, `profileImage`, `profileCover`) VALUES (:email, :password, :screenName, 'assets/images/ProfileImage.png', 'assets/images/CoverImage.png')");
+    $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmt->bindParam(":password", $passwordHash , PDO::PARAM_STR);
+    $stmt->bindParam(":screenName", $screenName, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $user_id = $this->pdo->lastInsertId();
+    $_SESSION['user_id'] = $user_id;
+  }
+
+// dit zorgt ervoor dat userinfo getoond wordt in home.php
+public function userData($user_id){
+    $stmt = $this->pdo->prepare('SELECT * FROM `users` WHERE `user_id` = :user_id');
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_OBJ);
+}
+
 }
 ?>
--- Includes/Register.php
+++ Includes/Register.php
@@ -1,5 +1,38 @@
+<?php
+ 
+if(isset($_POST['signup'])){
+	$screenName = $_POST['screenName'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$error = '';
+
+	if(empty($screenName) or empty($password) or empty($email)){
+		$error = 'All fields are required';
+	}else {
+		$email = $getFromU->checkInput($email);
+		$screenName = $getFromU->checkInput($screenName);
+		$password = $getFromU->checkInput($password);
+
+		if(!filter_var($email)) {
+			$error = 'Invalid email format';
+		}else if(strlen($screenName) > 20){
+			$error = 'Name must be between in 6-20 characters';
+		}else if(strlen($password) < 5){
+			$error = 'Password is too short';
+		}else {
+			if($getFromU->checkEmail($email) === true){
+				$error = 'Email is already in use';
+			}else {
+				$getFromU->register($email, $password , $screenName);
+				
+				header('Location: home.php');
+			}
+		}
+	}
+}
+?>
 <form method="post">
-<div class="signup-div"> 
+<div class="signup-div">
 	<h3>Sign up </h3>
 	<ul>
 		<li>
@@ -12,13 +45,16 @@
 			<input type="password" name="password" placeholder="Password"/>
 		</li>
 		<li>
-			<input type="submit" name="signup" Value="Sign Up">
+			<input type="submit" name="signup" Value="Signup">
 		</li>
+		<?php
+      if(isset($error)){
+        echo '<li class="error-li">
+        <div class="span-fp-error">'.$error.'</div>
+        </li>';
+      }
+    ?>
 	</ul>
-	<!--
-	 <li class="error-li">
-	  <div class="span-fp-error"></div>
-	 </li> 
-	-->
+
 </div>
 </form>
